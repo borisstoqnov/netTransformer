856fff69d2f6625bac546700a5899a27226eb77b
ntransformer <vasil-yordanov@users.noreply.github.com>
2012-06-15 21:26:36 +0000

    fix iDcisover.sh to work under linux

--- iDiscover/conf/bat/iDiscover.sh
+++ iDiscover/conf/bat/iDiscover.sh
@@ -3,13 +3,8 @@
 #Discovery Startup Script
 echo "If there is a problem! Please enter the correct path to your java"
 
-
-
-
 LIB=../lib
-DIST=../lib
-CLASSPATH=.:${DIST}/discovery-manager.jar:${DIST}/utils.jar:${DIST}/snmp-discoverer.jar:${DIST}/resource-manager.jar:${LIB}/snmp4j/snmp4j-1.11.2.jar:${LIB}/mibble/mibble-mibs-2.9.2.jar:${LIB}/mibble/grammatica-1.5.jar:${LIB}/mibble/mibble-parser-2.9.2.jar:${LIB}/log4j/log4j-1.2.14.jar:${LIB}/apache-commons/commons-io-2.0.1.jar:${LIB}/apache-commons/commons-beanutils-1.8.3.jar:${LIB}/apache-commons/commons-logging-1.1.1.jar:${LIB}/saxon/saxon9.jar
-echo $CLASSPATH
-echo "Fire up discovery Process"
-java -classpath ${CLASSPATH} -Xms256m -Xmx512m com.discovery.core.DiscoveryManager -m ../mibs -f discovery-manager/conf/xml/discoveryManager.xml $*
-
+CLASSPATH=".:${LIB}/iDiscover-${version}.jar:${LIB}/utils-${version}.jar:${LIB}/snmp2xml-${version}.jar:${LIB}/resourceManager-${version}.jar:${LIB}/snmp4j-1.11.2.jar:${LIB}/mibble-mibs-2.9.2.jar:${LIB}/grammatica-1.5.jar:${LIB}/mibble-parser-2.9.2.jar:${LIB}/log4j-1.2.16.jar:${LIB}/commons-io-1.3.2.jar:${LIB}/commons-beanutils-1.8.3.jar:${LIB}/commons-logging-1.1.1.jar:${LIB}/saxon-9.1.0.8.jar"
+#DEBUG_JAVA_OPTS=-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005
+JAVA_OPTS=-Xms256m -Xmx512m ${DEBUG_JAVA_OPTS}
+java ${JAVA_OPTS} -classpath ${CLASSPATH} net.itransformers.idiscover.core.DiscoveryManager -m ..\mibs -f iDiscover/conf/xml/discoveryManager.xml %*
