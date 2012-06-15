58582cd35573ebd920bc5f57e78825f8eb569a97
ntransformer <vasil-yordanov@users.noreply.github.com>
2012-06-15 21:37:37 +0000

    fixed topology viewer scripts to work under linux. small fix for saxon under win

--- iTopologyManager/topologyViewer/conf/bat/u_topoManager.sh
+++ iTopologyManager/topologyViewer/conf/bat/u_topoManager.sh
@@ -3,8 +3,5 @@
 echo "Echo"
 LIB=../lib
 
-CLASSPATH="..:${LIB}/utils.jar:${LIB}/saxon/saxon9.jar:${LIB}/topology-viewer.jar:${LIB}/rightclick.jar:${LIB}/dump/dump.jar:${LIB}/fulfilment-factory.jar:${LIB}/parameter-factory.jar:${LIB}/resource-manager.jar:${LIB}/jta26/jta26.jar:${LIB}/jsch/jsch-0.1.44.jar:${LIB}/gritty/gritty-0.02.jar:${LIB}/commons-net-3.0.1/commons-net-3.0.1.jar:${LIB}/log4j/log4j-1.2.14.jar:${LIB}/apache-commons/commons-io-2.0.1.jar:${LIB}/jung/collections-generic-4.01.jar:${LIB}/jung/colt-1.2.0.jar:${LIB}/jung/concurrent-1.3.4.jar:$LIB/jung/j3d-core-1.3.1.jar:${LIB}/jung/jung-3d-2.0.1.jar:${LIB}/jung/jung-3d-demos-2.0.1.jar:${LIB}/jung/jung-algorithms-2.0.1.jar:${LIB}/jung/jung-api-2.0.1.jar:${LIB}/jung/jung-graph-impl-2.0.1.jar:${LIB}/jung/jung-io-2.0.1.jar:${LIB}/jung/jung-jai-2.0.1.jar:${LIB}/jung/jung-jai-samples-2.0.1.jar:${LIB}/jung/jung-samples-2.0.1.jar:${LIB}/jung/jung-visualization-2.0.1.jar:${LIB}/jung/stax-api-1.0.1.jar:${LIB}/jung/vecmath-1.3.1.jar:${LIB}/jung/wstx-asl-3.2.6.jar:"
-echo $CLASSPATH
-echo $LIB
-java -classpath ${CLASSPATH} net.itransformers.topologyviewer.gui.TopologyViewer -t undirected -d network -g undirected -f topology-viewer/conf/xml/undirected.xml $*
-
+CLASSPATH=".:${LIB}/topologyViewer-${version}.jar:${LIB}/topologyManagerConfig-${version}.jar:${LIB}/screencap-1.0.jar:${LIB}/rightclickApi-${version}.jar:${LIB}/rightclick-${version}.jar:${LIB}/fulfilmentFactory-${version}.jar:${LIB}/parameterFactory-${version}.jar:${LIB}/resourceManager-${version}.jar:${LIB}/jta26\jta26.jar:${LIB}/jsch-0.1.44.jar:${LIB}/gritty-0.02.jar:${LIB}/commons-net-3.0.1.jar:${LIB}/log4j-1.2.16.jar:${LIB}/commons-io-2.0.1.jar:${LIB}/collections-generic-4.01.jar:${LIB}/colt-1.2.0.jar:${LIB}/concurrent-1.3.4.jar:${LIB}/jung-algorithms-2.0.1.jar:${LIB}/jung-api-2.0.1.jar:${LIB}/jung-graph-impl-2.0.1.jar:${LIB}/jung-io-2.0.1.jar:${LIB}/jung-visualization-2.0.1.jar:${LIB}/stax-api-1.0.1.jar:${LIB}/wstx-asl-3.2.6.jar:${LIB}/saxon-9.1.0.8.jar"
+java -classpath ${CLASSPATH}  net.itransformers.topologyviewer.gui.TopologyViewer -t undirected -d network -g undirected -f iTopologyManager/topologyViewer/conf/xml/undirected.xml $*
