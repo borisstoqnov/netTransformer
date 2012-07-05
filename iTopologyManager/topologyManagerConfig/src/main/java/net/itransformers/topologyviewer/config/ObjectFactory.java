60b16083388d70b53ffc34a934f0db9f4614e4f7
ntransformer <vasil-yordanov@users.noreply.github.com>
2012-07-05 12:30:26 +0000

    fixed fqn of java classes in config files

--- iTopologyManager/topologyManagerConfig/src/main/java/net/itransformers/topologyviewer/config/ObjectFactory.java
+++ iTopologyManager/topologyManagerConfig/src/main/java/net/itransformers/topologyviewer/config/ObjectFactory.java
@@ -17,7 +17,7 @@ import javax.xml.namespace.QName;
 /**
  * This object contains factory methods for each 
  * Java content interface and Java element interface 
- * generated in the com.topolgyviewer.config package. 
+ * generated in the net.itransformers.topologyviewer.config package.
  * <p>An ObjectFactory allows you to programatically 
  * construct new instances of the Java representation 
  * for XML content. The Java representation of XML 
@@ -34,7 +34,7 @@ public class ObjectFactory {
     private final static QName _TopologyViewerConf_QNAME = new QName("", "topology-viewer-conf");
 
     /**
-     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: com.topolgyviewer.config
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: net.itransformers.topologyviewer.config
      * 
      */
     public ObjectFactory() {
