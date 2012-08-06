c4908eef9ca6005fb256cdf2c57b6d086e123f79
ntransformer <vasil-yordanov@users.noreply.github.com>
2012-08-06 06:40:57 +0000

    added rest ws module.

---
+++ wsitransformer/src/main/java/net.itransformers.ws/rs/HelloWorldResource.java
@@ -0,0 +1,39 @@
+/*
+ * iMap is an open source tool able to upload Internet BGP peering information
+ *  and to visualize the beauty of Internet BGP Peering in 2D map.
+ *  Copyright (C) 2012  http://itransformers.net
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.itransformers.ws.rs;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+ import javax.ws.rs.Path;
+ 
+// The Java class will be hosted at the URI path "/helloworld"
+ @Path("/helloworld")
+ public class HelloWorldResource {
+ 
+     // The Java method will process HTTP GET requests
+     @GET
+     // The Java method will produce content identified by the MIME Media
+     // type "text/plain"
+     @Produces("text/plain")
+     public String getClichedMessage() {
+         // Return some cliched textual content
+         return "Hello World";
+     }
+ }
