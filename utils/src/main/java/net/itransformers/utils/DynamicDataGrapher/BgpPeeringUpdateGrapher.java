695da97373fc3ddfc456f61b217f4034445d3aeb
ntransformer <vasil-yordanov@users.noreply.github.com>
2013-12-06 11:09:49 +0000

    Fixed dependecy from apache-commons-io and added new test case for graphml merge tool

--- utils/src/main/java/net/itransformers/utils/DynamicDataGrapher/BgpPeeringUpdateGrapher.java
+++ utils/src/main/java/net/itransformers/utils/DynamicDataGrapher/BgpPeeringUpdateGrapher.java
@@ -1,10 +1,10 @@
 package net.itransformers.utils.DynamicDataGrapher;
 
-import net.itransformers.snmptoolkit.CmdOptions;
-import net.itransformers.snmptoolkit.CmdParser;
-import net.itransformers.snmptoolkit.Get;
-import net.itransformers.snmptoolkit.messagedispacher.DefaultMessageDispatcherFactory;
-import net.itransformers.snmptoolkit.transport.UdpTransportMappingFactory;
+//import net.itransformers.snmptoolkit.CmdOptions;
+//import net.itransformers.snmptoolkit.CmdParser;
+//import net.itransformers.snmptoolkit.Get;
+//import net.itransformers.snmptoolkit.messagedispacher.DefaultMessageDispatcherFactory;
+//import net.itransformers.snmptoolkit.transport.UdpTransportMappingFactory;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
@@ -30,204 +30,204 @@ import java.util.Map;
  * (random) data by clicking on a button.
  *
  */
-public class BgpPeeringUpdateGrapher extends ApplicationFrame implements ActionListener {
-
-    /** The time series data. */
-    private static TimeSeries series;
-
-    /** The most recent value added. */
-    private static long lastValue = 0;
-    private static Get snmpGet;
-
-
-
-    private static int deltaInterval;
-    /**
-     * Constructs a new demonstration application.
-     *
-     * @param title  the frame title.
-     */
-    public BgpPeeringUpdateGrapher(final String title,String series) {
-
-        super(title);
-        this.series = new TimeSeries(series, Millisecond.class);
-        final TimeSeriesCollection dataset = new TimeSeriesCollection(this.series);
-
-        final JFreeChart chart = createChart(dataset, title);
-
-        final ChartPanel chartPanel = new ChartPanel(chart);
-        chartPanel.setPreferredSize(new java.awt.Dimension(1000, 1000));
-        chartPanel.setMouseWheelEnabled(true);
-        chartPanel.setMouseZoomable(true);
-
-        final JPanel content = new JPanel(new BorderLayout());
-        content.add(chartPanel);
-        setContentPane(content);
-
-    }
-
-
-    private JFreeChart createChart(final XYDataset dataset, String chartTitle) {
-        final JFreeChart result = ChartFactory.createTimeSeriesChart(
-                chartTitle,
-                "Time",
-                "Value",
-                dataset,
-                true,
-                true,
-                false
-        );
-        final XYPlot plot = result.getXYPlot();
-        ValueAxis axis = plot.getDomainAxis();
-        axis.setAutoRange(true);
-        axis.setFixedAutoRange(60000.0);  // 60 seconds
-        axis = plot.getRangeAxis();
-        axis.setAutoRangeMinimumSize(1000.0);
-
-        return result;
-    }
-
-
-    public void actionPerformed(final ActionEvent e) {
-        if (e.getActionCommand().equals("ADD_DATA")) {
-           Long bgpInUpdate = null;
-            try {
-                bgpInUpdate = Long.parseLong(snmpGet.getSNMPValue());
-                System.out.println(bgpInUpdate);
-            } catch (IOException e1) {
-                e1.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-            }
-
-            long delta = bgpInUpdate - lastValue;
-            lastValue = bgpInUpdate;
-            System.out.println(delta);
-            final Millisecond now = new Millisecond();
-            System.out.println("Now = " + now.toString());
-            this.series.add(new Millisecond(), delta);
-        }
-    }
-
-//    private static Map<String, String> loadProperties(File file) throws IOException {
-//        Properties props = new Properties();
-//        props.load(new FileInputStream(file));
-//        HashMap<String, String> settings = new HashMap<String, String>();
-//        for (Object key : props.keySet()) {
-//            settings.put((String)key,(String)props.get(key));
+public class BgpPeeringUpdateGrapher {//extends ApplicationFrame implements ActionListener {
+//
+//    /** The time series data. */
+//    private static TimeSeries series;
+//
+//    /** The most recent value added. */
+//    private static long lastValue = 0;
+//    private static Get snmpGet;
+//
+//
+//
+//    private static int deltaInterval;
+//    /**
+//     * Constructs a new demonstration application.
+//     *
+//     * @param title  the frame title.
+//     */
+//    public BgpPeeringUpdateGrapher(final String title,String series) {
+//
+//        super(title);
+//        this.series = new TimeSeries(series, Millisecond.class);
+//        final TimeSeriesCollection dataset = new TimeSeriesCollection(this.series);
+//
+//        final JFreeChart chart = createChart(dataset, title);
+//
+//        final ChartPanel chartPanel = new ChartPanel(chart);
+//        chartPanel.setPreferredSize(new java.awt.Dimension(1000, 1000));
+//        chartPanel.setMouseWheelEnabled(true);
+//        chartPanel.setMouseZoomable(true);
+//
+//        final JPanel content = new JPanel(new BorderLayout());
+//        content.add(chartPanel);
+//        setContentPane(content);
+//
+//    }
+//
+//
+//    private JFreeChart createChart(final XYDataset dataset, String chartTitle) {
+//        final JFreeChart result = ChartFactory.createTimeSeriesChart(
+//                chartTitle,
+//                "Time",
+//                "Value",
+//                dataset,
+//                true,
+//                true,
+//                false
+//        );
+//        final XYPlot plot = result.getXYPlot();
+//        ValueAxis axis = plot.getDomainAxis();
+//        axis.setAutoRange(true);
+//        axis.setFixedAutoRange(60000.0);  // 60 seconds
+//        axis = plot.getRangeAxis();
+//        axis.setAutoRangeMinimumSize(1000.0);
+//
+//        return result;
+//    }
+//
+//
+//    public void actionPerformed(final ActionEvent e) {
+//        if (e.getActionCommand().equals("ADD_DATA")) {
+//           Long bgpInUpdate = null;
+//            try {
+//                bgpInUpdate = Long.parseLong(snmpGet.getSNMPValue());
+//                System.out.println(bgpInUpdate);
+//            } catch (IOException e1) {
+//                e1.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+//            }
+//
+//            long delta = bgpInUpdate - lastValue;
+//            lastValue = bgpInUpdate;
+//            System.out.println(delta);
+//            final Millisecond now = new Millisecond();
+//            System.out.println("Now = " + now.toString());
+//            this.series.add(new Millisecond(), delta);
 //        }
-//        return settings;
 //    }
-//    private static void printUsage(String param){
-//        System.out.println("Usage:   java net.itransformers.net.itransformers.utils.DynamicDataGrapher.BgpPeeringUpdateGrapher -s <Path to bgpUpdates.properties>");
-//        System.out.println("Example [Windows]: java net.itransformers.utils.DynamicDataGrapher.BgpPeeringUpdateGrapher -s utils\\conf\\bgpUpdates.properties");
-//        System.out.println("Example [Unix]: java net.itransformers.utils.DynamicDataGrapher.BgpPeeringUpdateGrapher -s utils/conf/bgpUpdates.properties");
-//        System.out.println("Missing parameter: "+param);
+//
+////    private static Map<String, String> loadProperties(File file) throws IOException {
+////        Properties props = new Properties();
+////        props.load(new FileInputStream(file));
+////        HashMap<String, String> settings = new HashMap<String, String>();
+////        for (Object key : props.keySet()) {
+////            settings.put((String)key,(String)props.get(key));
+////        }
+////        return settings;
+////    }
+////    private static void printUsage(String param){
+////        System.out.println("Usage:   java net.itransformers.net.itransformers.utils.DynamicDataGrapher.BgpPeeringUpdateGrapher -s <Path to bgpUpdates.properties>");
+////        System.out.println("Example [Windows]: java net.itransformers.utils.DynamicDataGrapher.BgpPeeringUpdateGrapher -s utils\\conf\\bgpUpdates.properties");
+////        System.out.println("Example [Unix]: java net.itransformers.utils.DynamicDataGrapher.BgpPeeringUpdateGrapher -s utils/conf/bgpUpdates.properties");
+////        System.out.println("Missing parameter: "+param);
+////    }
+//
+//    public static void main(final String[] args) throws IOException {
+//        Map<CmdOptions,String> opts;
+//
+//        try {
+//            opts = CmdParser.parseCmd(args);
+//        } catch (Exception e) {
+//            System.out.println(e.getMessage());
+//            CmdParser.printGetUsage();
+//            return;
+//        }
+//        String oid = opts.get(CmdOptions.OIDS);
+//        if (oid == null) {
+//            System.out.println("Missing option \"-"+ CmdOptions.OIDS.getName()+"\"");
+//            CmdParser.printGetUsage();
+//        }
+//        String address = opts.get(CmdOptions.ADDRESS);
+//        if (address == null) {
+//            System.out.println("Missing option \"-"+ CmdOptions.ADDRESS.getName()+"\"");
+//            CmdParser.printGetUsage();
+//        }
+//        String community = opts.get(CmdOptions.COMMUNITY);
+//        if (community == null) {
+//            System.out.println("Missing option \"-"+ CmdOptions.COMMUNITY.getName()+"\"");
+//            CmdParser.printGetUsage();
+//        }
+//        int versionInt;
+//        try {
+//            String version = opts.get(CmdOptions.VERSION);
+//            versionInt = Integer.parseInt(version);
+//        } catch (NumberFormatException nfe) {
+//            System.out.println("Invalid parameter value for \"-"+ CmdOptions.VERSION + "\", int value is required");
+//            CmdParser.printGetUsage();
+//            return;
+//        }
+//        int retriesInt;
+//        try {
+//            String retries = opts.get(CmdOptions.RETRIES);
+//            retriesInt = Integer.parseInt(retries);
+//        } catch (NumberFormatException nfe) {
+//            System.out.println("Invalid parameter value for \"-"+ CmdOptions.RETRIES + "\", int value is required");
+//            CmdParser.printGetUsage();
+//            return;
+//        }
+//        int timeoutInt;
+//        try {
+//            String timeout = opts.get(CmdOptions.TIMEOUT);
+//            timeoutInt = Integer.parseInt(timeout);
+//        } catch (NumberFormatException nfe) {
+//            System.out.println("Invalid parameter value for \"-"+ CmdOptions.TIMEOUT + "\", int value is required");
+//            CmdParser.printGetUsage();
+//            return;
+//        }
+//        try {
+//            String delta = opts.get(CmdOptions.DELTA);
+//            deltaInterval = Integer.parseInt(delta);
+//            if (deltaInterval < 600000){
+//                System.out.println("Delta interval "+ deltaInterval + " is too small. It should be a number higher than 600000 ms (10 minutes)!");
+//                return;
+//            }
+//        } catch (NumberFormatException nfe) {
+//            System.out.println("Invalid parameter value for \"-"+ CmdOptions.DELTA + "\", int value is required");
+//            CmdParser.printGetUsage();
+//            return;
+//        }
+//        snmpGet = new Get(oid, address, versionInt, retriesInt, timeoutInt, community, new UdpTransportMappingFactory(), new DefaultMessageDispatcherFactory());
+//        String response =   snmpGet.getSNMPValue();
+//        System.out.println(response);
+//        lastValue = Long.parseLong(response);
+//        final BgpPeeringUpdateGrapher demo = new BgpPeeringUpdateGrapher("BgpPeeringUpdate Line Chart",oid);
+//        demo.pack();
+//        gen myGen = new gen();
+//        new Thread(myGen).start();
+//        RefineryUtilities.centerFrameOnScreen(demo);
+//        demo.setVisible(true);
+//
 //    }
-
-    public static void main(final String[] args) throws IOException {
-        Map<CmdOptions,String> opts;
-
-        try {
-            opts = CmdParser.parseCmd(args);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            CmdParser.printGetUsage();
-            return;
-        }
-        String oid = opts.get(CmdOptions.OIDS);
-        if (oid == null) {
-            System.out.println("Missing option \"-"+ CmdOptions.OIDS.getName()+"\"");
-            CmdParser.printGetUsage();
-        }
-        String address = opts.get(CmdOptions.ADDRESS);
-        if (address == null) {
-            System.out.println("Missing option \"-"+ CmdOptions.ADDRESS.getName()+"\"");
-            CmdParser.printGetUsage();
-        }
-        String community = opts.get(CmdOptions.COMMUNITY);
-        if (community == null) {
-            System.out.println("Missing option \"-"+ CmdOptions.COMMUNITY.getName()+"\"");
-            CmdParser.printGetUsage();
-        }
-        int versionInt;
-        try {
-            String version = opts.get(CmdOptions.VERSION);
-            versionInt = Integer.parseInt(version);
-        } catch (NumberFormatException nfe) {
-            System.out.println("Invalid parameter value for \"-"+ CmdOptions.VERSION + "\", int value is required");
-            CmdParser.printGetUsage();
-            return;
-        }
-        int retriesInt;
-        try {
-            String retries = opts.get(CmdOptions.RETRIES);
-            retriesInt = Integer.parseInt(retries);
-        } catch (NumberFormatException nfe) {
-            System.out.println("Invalid parameter value for \"-"+ CmdOptions.RETRIES + "\", int value is required");
-            CmdParser.printGetUsage();
-            return;
-        }
-        int timeoutInt;
-        try {
-            String timeout = opts.get(CmdOptions.TIMEOUT);
-            timeoutInt = Integer.parseInt(timeout);
-        } catch (NumberFormatException nfe) {
-            System.out.println("Invalid parameter value for \"-"+ CmdOptions.TIMEOUT + "\", int value is required");
-            CmdParser.printGetUsage();
-            return;
-        }
-        try {
-            String delta = opts.get(CmdOptions.DELTA);
-            deltaInterval = Integer.parseInt(delta);
-            if (deltaInterval < 600000){
-                System.out.println("Delta interval "+ deltaInterval + " is too small. It should be a number higher than 600000 ms (10 minutes)!");
-                return;
-            }
-        } catch (NumberFormatException nfe) {
-            System.out.println("Invalid parameter value for \"-"+ CmdOptions.DELTA + "\", int value is required");
-            CmdParser.printGetUsage();
-            return;
-        }
-        snmpGet = new Get(oid, address, versionInt, retriesInt, timeoutInt, community, new UdpTransportMappingFactory(), new DefaultMessageDispatcherFactory());
-        String response =   snmpGet.getSNMPValue();
-        System.out.println(response);
-        lastValue = Long.parseLong(response);
-        final BgpPeeringUpdateGrapher demo = new BgpPeeringUpdateGrapher("BgpPeeringUpdate Line Chart",oid);
-        demo.pack();
-        gen myGen = new gen();
-        new Thread(myGen).start();
-        RefineryUtilities.centerFrameOnScreen(demo);
-        demo.setVisible(true);
-
-    }
-    static class gen implements Runnable {
-
-        public void run() {
-            while(true) {
-                Long update = null;
-
-                try {
-                    update = Long.parseLong(snmpGet.getSNMPValue());
-                } catch (IOException e1) {
-                    e1.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-                }
-
-                long delta = update - lastValue;
-                System.out.println("Update: "+ update);
-
-                System.out.println("previous Update: "+ lastValue);
-                System.out.println("delta: "+ delta);
-
-                lastValue = update;
-
-
-                series.addOrUpdate(new Millisecond(), delta);
-                try {
-                    Thread.sleep(deltaInterval);
-                } catch (InterruptedException ex) {
-                    System.out.println(ex);
-                }
-            }
-        }
-    }
-
+//    static class gen implements Runnable {
+//
+//        public void run() {
+//            while(true) {
+//                Long update = null;
+//
+//                try {
+//                    update = Long.parseLong(snmpGet.getSNMPValue());
+//                } catch (IOException e1) {
+//                    e1.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+//                }
+//
+//                long delta = update - lastValue;
+//                System.out.println("Update: "+ update);
+//
+//                System.out.println("previous Update: "+ lastValue);
+//                System.out.println("delta: "+ delta);
+//
+//                lastValue = update;
+//
+//
+//                series.addOrUpdate(new Millisecond(), delta);
+//                try {
+//                    Thread.sleep(deltaInterval);
+//                } catch (InterruptedException ex) {
+//                    System.out.println(ex);
+//                }
+//            }
+//        }
+//    }
+//
 }
--- utils/src/test/java/net/itransformers/utils/graphmlmerge/GraphmlMergeTestCase.java
+++ utils/src/test/java/net/itransformers/utils/graphmlmerge/GraphmlMergeTestCase.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -23,6 +25,36 @@ public class GraphmlMergeTestCase {
         File f3 = new File("utils/src/test/java/net/itransformers/utils/graphmlmerge/3.graphml");
         File f3Actual = File.createTempFile("graphml_merge",".xml");
         new GrahmlMerge().merge(new File[]{f1, f2}, f3Actual);
+        String xml3 = FileUtils.readFileToString(f3);
+        String xml3Actual = FileUtils.readFileToString(f3Actual);
+        Assert.assertEquals(xml3, xml3Actual);
+    }
+    @Test
+    public void testMerge2() throws IOException {
+        File f1 = new File("utils/src/test/java/net/itransformers/utils/graphmlmerge/1.graphml");
+        File f2 = new File("utils/src/test/java/net/itransformers/utils/graphmlmerge/2.graphml");
+        File f3 = new File("utils/src/test/java/net/itransformers/utils/graphmlmerge/4.graphml");
+        File f3Actual = File.createTempFile("graphml_merge",".xml");
+
+        Map<String, MergeConflictResolver> vertexConflictResolver = new HashMap<String, MergeConflictResolver>();
+        vertexConflictResolver.put("name", new MergeConflictResolver(){
+            @Override
+            public Object resolveConflict(Object srcValue, Object targetValue) {
+                return srcValue + "-" + targetValue;
+            }
+        });
+        Map<String, MergeConflictResolver> edgeConflictResolver = new HashMap<String, MergeConflictResolver>();
+        edgeConflictResolver.put("weight", new MergeConflictResolver() {
+            @Override
+            public Object resolveConflict(Object srcValue1, Object targetValue) {
+                if (srcValue1 instanceof Float && targetValue instanceof Float) {
+                    return (Float)srcValue1 + (Float)targetValue;
+                } else {
+                    return srcValue1 + "#" + targetValue;
+                }
+            }
+        });
+        new GrahmlMerge(edgeConflictResolver, vertexConflictResolver).merge(new File[]{f1, f2}, f3Actual);
         String xml3 = FileUtils.readFileToString(f3);
         String xml3Actual = FileUtils.readFileToString(f3Actual);
         Assert.assertEquals(xml3, xml3Actual);
