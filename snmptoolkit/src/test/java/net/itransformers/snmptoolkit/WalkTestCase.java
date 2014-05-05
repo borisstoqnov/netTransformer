81b7f1c1bb95c988382ab7d90ca072b708bb1b90
Nikolay Milovanov <nmilovanov@nbu.bg>
2014-05-05 13:59:47 +0000

    ciscoIPv6 test case added!

--- snmptoolkit/src/test/java/net/itransformers/snmptoolkit/WalkTestCase.java
+++ snmptoolkit/src/test/java/net/itransformers/snmptoolkit/WalkTestCase.java
@@ -18,7 +18,7 @@ import java.util.Properties;
 
 public class WalkTestCase {
     @Test
-    public void testWalk() throws MibLoaderException, ParserConfigurationException, SAXException, XPathExpressionException, IOException {
+    public void aptiloTestWalk() throws MibLoaderException, ParserConfigurationException, SAXException, XPathExpressionException, IOException {
         String oids = "radiusAuthServTotalAccessRequests,radiusAuthServTotalInvalidRequests,radiusAuthServTotalDupAccessRequests,radiusAuthServTotalAccessAccepts,radiusAuthServTotalAccessRejects,radiusAuthServTotalAccessChallenges,radiusAuthServTotalMalformedAccessRequests,radiusAuthServTotalBadAuthenticators,radiusAuthServTotalPacketsDropped,radiusAuthServTotalUnknownTypes,radiusAuthClientAddress,radiusAuthClientID,radiusAuthServAccessRequests,radiusAuthServDupAccessRequests," +
                 "radiusAuthServAccessAccepts,radiusAuthServAccessRejects," +
                 "radiusAuthServAccessChallenges,radiusAuthServMalformedAccessRequests," +
@@ -59,7 +59,7 @@ public class WalkTestCase {
 
         HashMap<CmdOptions, String> cmdOptions = new HashMap<CmdOptions, String>();
         cmdOptions.put(CmdOptions.MIBS_DIR,mibDir);
-        cmdOptions.put(CmdOptions.ADDRESS,"localhost/1161");
+        cmdOptions.put(CmdOptions.ADDRESS,"localhost/11161");
         cmdOptions.put(CmdOptions.COMMUNITY,"aptiloAll");
         cmdOptions.put(CmdOptions.VERSION,"2c");
         cmdOptions.put(CmdOptions.TIMEOUT,"1000");
@@ -73,6 +73,29 @@ public class WalkTestCase {
         Node root = walker.walk(oids.split(","), parameters);
         String xml = Walk.printTreeAsXML(root, true);
         String expectedXML = FileUtils.readFileToString(new File("snmptoolkit/src/test/java/resources/aptilo.xml"));
+        Assert.assertEquals(expectedXML,xml);
+    }
+    @Test
+    public void ciscoTestWalk() throws MibLoaderException, ParserConfigurationException, SAXException, XPathExpressionException, IOException {
+        String oids = "ipv6Forwarding, ipv6IfIndex,ipv6AddrEntry,ipv6NetToMediaEntry,ipv6RouteEntry,cIpAddressEntry";
+        String mibDir = "snmptoolkit/mibs";
+
+        HashMap<CmdOptions, String> cmdOptions = new HashMap<CmdOptions, String>();
+        cmdOptions.put(CmdOptions.MIBS_DIR,mibDir);
+        cmdOptions.put(CmdOptions.ADDRESS,"localhost/11161");
+        cmdOptions.put(CmdOptions.COMMUNITY,"ciscoIp");
+        cmdOptions.put(CmdOptions.VERSION,"2c");
+        cmdOptions.put(CmdOptions.TIMEOUT,"1000");
+        cmdOptions.put(CmdOptions.RETRIES,"100");
+        cmdOptions.put(CmdOptions.MAX_REPETITIONS,"100");
+        cmdOptions.put(CmdOptions.OIDS,oids);
+
+        Properties parameters = new Properties();
+        Walk.fillParams(cmdOptions, parameters);
+        Walk walker = new Walk(new File(mibDir), false, new UdpTransportMappingFactory(), new DefaultMessageDispatcherFactory());
+        Node root = walker.walk(oids.split(","), parameters);
+        String xml = Walk.printTreeAsXML(root, true);
+        String expectedXML = FileUtils.readFileToString(new File("snmptoolkit/src/test/java/resources/cisco_ipv6.xml"));
         Assert.assertEquals(expectedXML,xml);
     }
 }
