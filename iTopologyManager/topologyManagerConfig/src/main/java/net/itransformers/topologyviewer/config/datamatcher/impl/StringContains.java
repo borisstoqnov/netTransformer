04a55321c0a194e08b281405bbf283994c1ae993
niau <n.milovanov@gmail.com>
2014-10-27 10:36:15 +0200

    Modifing rules and matches

---
+++ iTopologyManager/topologyManagerConfig/src/main/java/net/itransformers/topologyviewer/config/datamatcher/impl/StringContains.java
@@ -0,0 +1,55 @@
+/*
+ * iTransformer is an open source tool able to discover and transform
+ *  IP network infrastructures.
+ *  Copyright (C) 2012  http://itransformers.net
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.itransformers.topologyviewer.config.datamatcher.impl;/*
+ * iTransformer is an open source tool able to discover IP networks
+ * and to perform dynamic data data population into a xml based inventory system.
+ * Copyright (C) 2010  http://itransformers.net
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import net.itransformers.topologyviewer.config.datamatcher.DataMatcher;
+import org.apache.log4j.Logger;
+
+public class StringContains implements DataMatcher {
+    static Logger logger = Logger.getLogger(StringContains.class);
+
+    @Override
+    public boolean compareData(String data1, String data2) {
+        if (data1 != null) {
+            logger.debug("EQ: "+data1+"="+data2);
+
+            return data1.equals(data2);
+        } else {
+            return false;
+        }
+    }
+}
---
+++ iTopologyManager/topologyViewer/src/main/java/net/itransformers/topologyviewer/nodetooltip/IntetMapHTMLCSVNodeTooltipTransformer.java
@@ -0,0 +1,55 @@
+/*
+ * netTransformer is an open source tool able to discover IP networks
+ * and to perform dynamic data data population into a xml based inventory system.
+ * Copyright (C) 2010  http://itransformers.net
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.itransformers.topologyviewer.nodetooltip;
+
+import net.itransformers.topologyviewer.config.TooltipType;
+import edu.uci.ics.jung.io.GraphMLMetadata;
+import org.apache.commons.collections15.Transformer;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class IntetMapHTMLCSVNodeTooltipTransformer extends NodeTooltipTransformerBase{
+    public IntetMapHTMLCSVNodeTooltipTransformer(TooltipType tooltipType, Map<String, GraphMLMetadata<String>> nodeMetadatas) {
+        super(tooltipType, nodeMetadatas);
+    }
+
+    public String transform(String node) {
+        try {
+             StringBuilder sb = new StringBuilder();
+            sb.append("<html>");
+             Set<String> valueSet = new HashSet<String>();
+             GraphMLMetadata<String> stringGraphMLMetadata = nodeMetadatas.get(tooltipType.getDataKey());
+             Transformer<String, String> transformer = stringGraphMLMetadata.transformer;
+             final String value = transformer.transform(node);
+             if (value != null && !sb.toString().contains(value)){
+                 sb.append(value);
+             }
+            sb.append(valueSet);
+
+            sb.append("</html>");
+            return sb.toString().replaceAll("\\[\\]","");
+        } catch (RuntimeException rte) {
+            rte.printStackTrace();
+            throw rte;
+        }
+    }
+}
