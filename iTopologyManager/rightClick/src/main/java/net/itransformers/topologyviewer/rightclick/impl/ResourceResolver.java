4034d27f509289a5f6ad1f3421bdd6aebf600dfb
Nick Milovanov <nmilovanov@sevone.com>
2016-05-14 22:09:50 +0300

    Fix #28 it will return a li

--- iTopologyManager/rightClick/src/main/java/net/itransformers/topologyviewer/rightclick/impl/ResourceResolver.java
+++ iTopologyManager/rightClick/src/main/java/net/itransformers/topologyviewer/rightclick/impl/ResourceResolver.java
@@ -28,6 +28,7 @@ import net.itransformers.resourcemanager.config.ResourceType;
 
 import java.io.File;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ResourceResolver {
@@ -44,6 +45,28 @@ public class ResourceResolver {
     }
 
     public static Map<String, String> getConnectionParams(ResourceType resource, Map<String, String> graphMLParams, String protocol) {
+
+        List<ConnectionParamsType> connParamsTypes = resource.getConnectionParams();
+
+        for (ConnectionParamsType connParamsType : connParamsTypes) {
+
+            if (connParamsType.getConnectionType().equals(protocol)){
+
+                Map<String, String> connParams = new HashMap<String, String>();
+                for (ParamType param : connParamsType.getParam()) {
+                    connParams.put(param.getName(), param.getValue());
+                }
+
+                connParams.put("discoveredIPv4Address", graphMLParams.get("discoveredIPv4Address"));
+                connParams.put("protocol", connParamsType.getConnectionType());
+                return connParams;
+            }
+        }
+
+
+
+
+
         if(resource.getConnectionParams()!=null){
             ConnectionParamsType connParamsType = resource.getConnectionParams().get(0);
             Map<String, String> connParams = new HashMap<String, String>();
