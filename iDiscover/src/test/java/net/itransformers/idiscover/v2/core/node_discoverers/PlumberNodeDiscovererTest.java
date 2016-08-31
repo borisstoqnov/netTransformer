7748b0230759bb5bf76c442c4670f4146d5df13f
Nick Milovanov <nmilovanov@sevone.com>
2016-08-31 21:51:45 +0300

    Adding Plumber Discoverer able to pipe a icmp, dns and snmp node discoverers together

---
+++ iDiscover/src/test/java/net/itransformers/idiscover/v2/core/node_discoverers/PlumberNodeDiscovererTest.java
@@ -0,0 +1,28 @@
+package net.itransformers.idiscover.v2.core.node_discoverers;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by niau on 8/31/16.
+ */
+public class PlumberNodeDiscovererTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testDiscover() throws Exception {
+
+    }
+}
---
+++ iDiscover/src/test/java/net/itransformers/idiscover/v2/core/node_discoverers/icmp/IcmpDiscovererTest.java
@@ -0,0 +1,10 @@
+package net.itransformers.idiscover.v2.core.node_discoverers.icmp;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by niau on 8/31/16.
+ */
+public class IcmpDiscovererTest {
+
+}
