db96324cc01c1272c231e0fcee1c140ce5de830f
Vasil Yordanov <vasil.yordanov@gmail.com>
2016-09-26 23:17:29 +0300

    Unified groupId of the pom files

--- ideas/src/main/java/net/itransformers/ws/upload/ObjectFactory.java
+++ ideas/src/main/java/net/itransformers/ws/upload/ObjectFactory.java
@@ -36,6 +36,22 @@ public class ObjectFactory {
     }
 
     /**
+     * Create an instance of {@link Node.Attributes }
+     * 
+     */
+    public Node.Attributes createNodeAttributes() {
+        return new Node.Attributes();
+    }
+
+    /**
+     * Create an instance of {@link Exception }
+     * 
+     */
+    public Exception createException() {
+        return new Exception();
+    }
+
+    /**
      * Create an instance of {@link ImportNode }
      * 
      */
@@ -52,35 +68,19 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link Node }
-     * 
-     */
-    public Node createNode() {
-        return new Node();
-    }
-
-    /**
-     * Create an instance of {@link Exception }
-     * 
-     */
-    public Exception createException() {
-        return new Exception();
-    }
-
-    /**
-     * Create an instance of {@link Node.Attributes }
-     * 
-     */
-    public Node.Attributes createNodeAttributes() {
-        return new Node.Attributes();
-    }
-
-    /**
      * Create an instance of {@link ImportNodeResponse }
      * 
      */
     public ImportNodeResponse createImportNodeResponse() {
         return new ImportNodeResponse();
+    }
+
+    /**
+     * Create an instance of {@link Node }
+     * 
+     */
+    public Node createNode() {
+        return new Node();
     }
 
     /**
