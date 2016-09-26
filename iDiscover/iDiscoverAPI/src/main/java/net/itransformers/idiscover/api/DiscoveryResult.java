fc6e87a236a114f9dcada782fc3c18c71e03823e
Nick Milovanov <nmilovanov@sevone.com>
2016-09-26 23:08:20 +0300

    Adding xmlNodeDataProvider

--- iDiscover/iDiscoverAPI/src/main/java/net/itransformers/idiscover/api/DiscoveryResult.java
+++ iDiscover/iDiscoverAPI/src/main/java/net/itransformers/idiscover/api/DiscoveryResult.java
@@ -7,6 +7,9 @@ import net.itransformers.idiscover.api.models.node_data.DiscoveredDeviceData;
  * Created by vasko on 9/18/2016.
  */
 public interface DiscoveryResult {
-    GraphmlGraph getGraphmlData(String nodeId);
-    DiscoveredDeviceData getNodeData(String nodeId);
+    String[] getDiscoveredVersions();
+    String getCreationDate(String version);
+    GraphmlGraph getVersionNetworkCentricModel(String version, String nodeId);
+    GraphmlGraph getNodeCentricNetworkModel(String version, String nodeId);
+    DiscoveredDeviceData getDeviceHierarchicalModel(String version, String nodeId);
 }
