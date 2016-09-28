c6e8c365afaf5f50725f17420504898f21e470d3
Nick Milovanov <nmilovanov@sevone.com>
2016-09-28 23:59:12 +0300

    Adding a working wsdiscovery restful api data provider!

--- wsitransformer/src/main/java/net.itransformers.ws/iDiscover/DiscoveryDataProvider.java
+++ wsitransformer/src/main/java/net.itransformers.ws/iDiscover/DiscoveryDataProvider.java
@@ -1,13 +1,11 @@
 package net.itransformers.ws.iDiscover;
 
-import net.itransformers.idiscover.api.DiscoveryResult;
 import net.itransformers.idiscover.api.models.graphml.GraphmlGraph;
+import net.itransformers.idiscover.api.models.graphml.GraphmlNode;
 import net.itransformers.idiscover.api.models.node_data.DiscoveredDeviceData;
+import net.itransformers.xmlNodeDataProvider.XmlNodeDataProvider;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -20,38 +18,54 @@ import java.util.List;
 @RequestMapping(value="/discovery")
 public class DiscoveryDataProvider {
 
-    @Resource(name="discoveryDataProvider")
-    private DiscoveryResult discoveryResult;
+    @Resource(name="xmlNodeDataProvider")
+    private XmlNodeDataProvider xmlNodeDataProvider;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     @ResponseBody
     List<String> getDiscoveredVersions(){
 
-       return discoveryResult.getDiscoveredVersions();
+       return xmlNodeDataProvider.getDiscoveredVersions();
 
     }
     @RequestMapping(value = "/{version}", method=RequestMethod.GET)
     @ResponseBody
     String getCreationDate(@PathVariable String version){
-        return discoveryResult.getCreationDate(version);
+        return xmlNodeDataProvider.getCreationDate(version);
     }
 
-    @RequestMapping(value = "/{version}/{nodeId}/networkcentric", method=RequestMethod.GET)
+    @RequestMapping(value = "/{version}/network", method=RequestMethod.GET)
     @ResponseBody
-    GraphmlGraph getVersionNetworkCentricModel(@PathVariable String version, @PathVariable String nodeId){
-        return discoveryResult.getVersionNetworkCentricModel(version,nodeId);
+    GraphmlGraph getNetwork(@PathVariable String version){
+        return xmlNodeDataProvider.getNetwork(version);
     }
-    @RequestMapping(value = "/{version}/{nodeId}/nodecentric", method=RequestMethod.GET)
+
+    @RequestMapping(value = "/{version}/network?nodeId={nodeId}", method=RequestMethod.GET)
     @ResponseBody
-    GraphmlGraph getNodeCentricNetworkModel(@PathVariable String version, @PathVariable String nodeId){
-        return discoveryResult.getNodeCentricNetworkModel(version, nodeId);
+    GraphmlNode getNode(@PathVariable String version, @RequestParam(value="nodeId", required=false) String nodeId){
+        return xmlNodeDataProvider.getNode(version, nodeId);
 
     }
-    @RequestMapping(value = "/{version}/{nodeId}/devicehierarchical", method=RequestMethod.GET)
+
+    @RequestMapping(value = "/{version}/nodes", method=RequestMethod.GET)
+    @ResponseBody
+
+    List<String> getDiscoveredNodes(@PathVariable String version){
+        return xmlNodeDataProvider.getDiscoveredNodes(version);
+    }
+
+    @RequestMapping(value = "/{version}/nodes/{nodeId}/data", method=RequestMethod.GET)
     @ResponseBody
 
     DiscoveredDeviceData getDeviceHierarchicalModel(@PathVariable String version, @PathVariable String nodeId){
-         return discoveryResult.getDeviceHierarchicalModel(version,nodeId);
+         return xmlNodeDataProvider.getDeviceHierarchicalModel(version, nodeId);
+    }
+
+
+    @RequestMapping(value = "/{version}/nodes/{nodeId}/raw", method=RequestMethod.GET)
+    @ResponseBody
+    String getRawData(@PathVariable String version, @PathVariable String nodeId){
+        return new String(xmlNodeDataProvider.getRawData(version, nodeId).getData());
     }
 
 
